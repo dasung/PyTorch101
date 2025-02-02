7ef684a23389899b4be02338c9af89e657e9f5e3
DasunG <Dasun.Gunasekara@lseg.com>
2025-02-02 13:41:11 +0530

    Updated env.yml

--- HelloTensor.py
+++ HelloTensor.py
@@ -1,27 +1,29 @@
-#%% package
-import tourch
-import numpy as 
-import seaborn as ans
+#%% packages
+import torch
+import seaborn as sns
+import numpy as np
 
 #%% create a tensor
+x = torch.tensor(5.5)
 
-x = tourch.tensor(5.5)
-
-#%% simple calculation
+# %% simple calculations
 y = x + 10
 print(y)
 
-#%%
+# %% automatic gradient calculation
+print(x.requires_grad)  # check if requires_grad is true, false if not directly specified
+
+x.requires_grad_() # set requires grad to true, default True
+
+#%% or set the flag directly during creation
+x = torch.tensor(2.0, requires_grad=True)
 print(x.requires_grad)
 
-x = tourch.tensor(2.0, requires_grad=True)
-print(x.requires_grad)
-
-#%%
+#%% function for showing automatic gradient calculation
 def y_function(val):
     return (val-3) * (val-6) * (val-4)
 
-x_range = np.linspace(0, 10, 101)
+x_range = np.linspace(0, 10, 101) # define range with 101 points for validation perpose
 y_range = [y_function(i) for i in x_range]
 
-sns.lineplot(x=x_range, y=y_range)
+sns.lineplot(x = x_range, y = y_range) # plot sample data
--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # PyTorch101
 
-    $ conda env create -f pytorch.yml
+first time only command to create env:
+    $ conda env create -f env.yml
+
+after manual activation:
+    $ conda activate PyEnv101
 
 ## Environment Installation from scratch
 
 install the environment manually by running these commands:
-    [ec2-user@ip-10-97-165-246 PyTorch101]$ conda create -n pytorch python=3.10
-    [ec2-user@ip-10-97-165-246 PyTorch101]$ conda activate pytorch
-    [ec2-user@ip-10-97-165-246 PyTorch101]$ conda install ipykernel
+
+* [ec2@ip-10-97-3-33 pyuser]$ conda create -n pytorch python=3.10
+* [ec2@ip-10-97-3-33 pyuser]$ conda activate pytorch
+* [ec2@ip-10-97-3-33 pyuser]$ conda install ipykernel
